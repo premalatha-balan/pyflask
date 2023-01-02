a78f966ea72b05d399468b021de92abc1928c6a6
Premalatha Balan <premalatha_balan@yahoo.co.uk>
2023-01-02 10:02:40 +0000

    halfway through hyp listing

--- app.py
+++ app.py
@@ -2,9 +2,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+HYPS = [
+  {
+    'id': 1,
+    'title': 'team iteration',
+    'org_level': 'team',
+    'prod_fl_level': 'delivery',
+    'Biz value': 'feedback',
+    'frequency': '2 weeks'
+  },
+  {
+    'id': 2,
+    'title': 'leadership updates',
+    'org_level': 'leadership',
+    'prod_fl_level': 'vision',
+    'Biz value': 'steer',
+    'frequency': 'weekly'
+  },
+  {
+    'id': 3,
+    'title': 'middle layer coordination',
+    'org_level': 'middle layer',
+    'prod_fl_level': 'links and dependencies',
+    'Biz value': 'coordination',
+    'frequency': 'weekly'
+  },
+]
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', hyps=HYPS)
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
